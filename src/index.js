16784157c3155d6bd6fc3c4a65a35d23720b0673
ShaDoKaiKan <sha.alia95@gmail.com>
2020-03-17 11:26:48 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+    let min = 0;
+    if (!array) {
+      return 0;
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            min = array[i] < min ? array[i] : min;
+        }
+  
+    }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+    if (!array) {
+      return 0;
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > 0) {
+            max = array[i] > max ? array[i] : max;
+        }
+  
+    }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  let res = sum / array.length;
+  return res;
+  
 }
